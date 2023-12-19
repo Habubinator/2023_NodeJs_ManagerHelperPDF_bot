ab9df94fc9d040f2f4bdb8d4320676fa32ab4cd7
Habubinator <61784574+Habubinator@users.noreply.github.com>
2023-12-19 17:26:28 +0200

    Added png translation from pdf

--- index.js
+++ index.js
@@ -1,114 +1,168 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
 process.env["NTBA_FIX_350"] = 1;
 process.env["NTBA_FIX_319"] = 1;
 
 // replace the value below with the Telegram token you receive from @BotFather (token in config file)
-// const config = require('./config.json')
+const config = require("./config.json");
 const token = process.env["bot_token"] || config.bot_token;
 
 // Визов мережевих помилок при помилках телеграму
-require("http")
-require("https")
+require("http");
+require("https");
 
 const fs = require("fs"); // Для перевірки чи існує файл
-const ncp = require('ncp').ncp;
+const ncp = require("ncp").ncp;
 
-require('./keep_alive.js') // Для UpTimeRobot
+require("./keep_alive.js"); // Для UpTimeRobot
 
 let userResultMap = new Map();
 
 // Create a bot that uses 'polling' to fetch new updates
 // { command: '/replace_file', description: 'Поменять шаблон'},
 
-const bot = new TelegramBot(token, {polling: true});
-bot.setMyCommands([{ command: '/start', description: 'Запустить бота' },
-                   { command: '/new', description: 'Сформировать PDF' },
-                   { command: '/result', description: 'Получить PDF файл вывода'}
-])
+const bot = new TelegramBot(token, { polling: true });
+bot.setMyCommands([
+  { command: "/start", description: "Запустить бота" },
+  { command: "/new", description: "Сформировать PDF" },
+  { command: "/result", description: "Получить PDF файл вывода" },
+]);
 // Вынес в отдельный файл вопросы и слой отвечающий за изменения PDF
 // let default_state = require('./questions.json');
-const {replaceText} = require("./pdfchanger.js")
+const { replaceText } = require("./pdfchanger.js");
+
+// Отдельный файл который переносит PDF в картинку
+const { pdfToPic } = require("./pdf-to-pic.js");
+
+// Change in ./pdf-to-pic.js as well
+let defaultFolder = "./pdfs-pngs";
 
 // Вынес классы в отдельный файл, отвечают за хранение и доступ к данным
-const {ChatState,Chats} = require("./classes.js")
+const { ChatState, Chats } = require("./classes.js");
 let chats = new Chats();
 
-async function changePDF(chatId, replacements){
+async function changePDF(chatId, replacements) {
   // default_state.replacements for debugging
-  await replaceText('./input.pdf', "./result"+chatId+".pdf", replacements)
-  userResultMap.set(chatId,replacements[0].replace)
+  userResultMap.set(chatId, replacements[0].replace);
+  await replaceText(
+    "./input.pdf",
+    defaultFolder + "/result" + chatId + ".pdf",
+    replacements
+  );
 }
 
-async function checkState(chatId, messageText){
+async function checkState(chatId, messageText) {
   try {
     let tempChat = chats.get(chatId);
-    if(tempChat.currentState != -1){
-      tempChat.states.replacements[tempChat.currentState].replace = messageText
+    if (tempChat.currentState != -1) {
+      tempChat.states.replacements[tempChat.currentState].replace = messageText;
     }
-    chats.next(chatId)
-    if(chats.isStatesEnded(chatId)){
-      changePDF(chatId, tempChat.states.replacements);
-      bot.sendMessage(chatId, "Документ сгенерирован успешно. \n/result - Для получения файла", {reply_markup: {remove_keyboard: true}})
-    }else{
+    chats.next(chatId);
+    if (chats.isStatesEnded(chatId)) {
+      let tempMessage = await bot.sendMessage(
+        chatId,
+        "Генерируем PDF документ..."
+      );
+      await changePDF(chatId, tempChat.states.replacements);
+      bot.editMessageText("Переводим из PDF в PNG...", {
+        chat_id: chatId,
+        message_id: tempMessage.message_id,
+      });
+      await pdfToPic(chatId, userResultMap.get(chatId));
+      await bot.deleteMessage(chatId, tempMessage.message_id);
+      bot.sendMessage(
+        chatId,
+        "Документ сгенерирован успешно. \n/result - Для получения файла",
+        { reply_markup: { remove_keyboard: true } }
+      );
+    } else {
       let options;
       // Правка по вводу данных, чтобы была таблица
-      if(tempChat.currentState == 1){
-        options = {reply_markup: {
-          keyboard: [
-            ['KAZAKHSTAN', 'TURKMENISTAN'],
-            ['UZBEKISTAN', 'KYRGYZSTAN'],
-            ['ARMENIA', 'GEORGIA'],
-            ["AZERBAIJAN", "BELARUS"],
-            ["TAJIKISTAN"]
-          ],resize_keyboard: true }}
+      if (tempChat.currentState == 1) {
+        options = {
+          reply_markup: {
+            keyboard: [
+              ["KAZAKHSTAN", "TURKMENISTAN"],
+              ["UZBEKISTAN", "KYRGYZSTAN"],
+              ["ARMENIA", "GEORGIA"],
+              ["AZERBAIJAN", "BELARUS"],
+              ["TAJIKISTAN"],
+            ],
+            resize_keyboard: true,
+          },
+        };
       }
-      bot.sendMessage(chatId, tempChat.states.questions[tempChat.currentState], options)
+      bot.sendMessage(
+        chatId,
+        tempChat.states.questions[tempChat.currentState],
+        options
+      );
     }
   } catch (error) {
-    console.log("Error: "+ error )
+    console.log("Error: " + error);
   }
 }
 
-async function stopState(chatId){
-  if(chats.checkIfChatStateTriggered(chatId)){
-    chats.remove(chatId)
+async function stopState(chatId) {
+  if (chats.checkIfChatStateTriggered(chatId)) {
+    chats.remove(chatId);
   }
 }
 
-bot.on('message', (msg) => {
+bot.on("message", (msg) => {
   try {
     const chatId = msg.chat.id;
     const text = msg.text;
-    switch(text){
-      case '/start':
-        stopState(chatId)
-        return bot.sendMessage(chatId, 'Это бот для упрощения менеджмента и ускорения ввода данных для  PDF документов \n/new - Создать новый документ \n/result - Вывести документ после создания', {reply_markup: {remove_keyboard: true}});
-      case '/result':
-        stopState(chatId)
+    switch (text) {
+      case "/start":
+        stopState(chatId);
+        return bot.sendMessage(
+          chatId,
+          "Это бот для упрощения менеджмента и ускорения ввода данных для  PDF документов \n/new - Создать новый документ \n/result - Вывести документ после создания",
+          { reply_markup: { remove_keyboard: true } }
+        );
+      case "/result":
+        stopState(chatId);
         try {
-          ncp('./result' + chatId + '.pdf', userResultMap.get(chatId) + '.pdf', (err) => {
-            if (err) {console.error("Error:", err);}
-          });
-          return bot.sendDocument(chatId, userResultMap.get(chatId)+".pdf", {caption: "Документ сгенерирован успешно!", reply_markup: {remove_keyboard: true}}).catch((err)=>{
-            console.error("Error:", err)
-          })
+          return bot
+            .sendDocument(
+              chatId,
+              defaultFolder + "/" + userResultMap.get(chatId) + ".png",
+              {
+                caption: "Документ сгенерирован успешно!",
+                reply_markup: { remove_keyboard: true },
+              }
+            )
+            .catch((err) => {
+              console.error("Error:", err);
+            });
         } catch (error) {
-          return bot.sendMessage(chatId, 'Вы ещё не создавали документ /new для создания нового документа',{reply_markup: {remove_keyboard: true}});
+          return bot.sendMessage(
+            chatId,
+            "Вы ещё не создавали документ /new для создания нового документа",
+            { reply_markup: { remove_keyboard: true } }
+          );
         }
         break;
-      case '/new':
-        stopState(chatId)
-        chats.add(chatId)
-        // Break відсутній, бо він відразу запускає ввід даних
+      case "/new":
+        stopState(chatId);
+        chats.add(chatId);
+      // Break відсутній, бо він відразу запускає ввід даних
       default:
-        if(!chats.checkIfChatStateTriggered(chatId)){
-          return bot.sendMessage(chatId, 'Вы не находитесь в процессе создания документа, воспользуйтесь /start для ознакомления с списком комманд',{reply_markup: {remove_keyboard: true}});
+        if (!chats.checkIfChatStateTriggered(chatId)) {
+          return bot.sendMessage(
+            chatId,
+            "Вы не находитесь в процессе создания документа, воспользуйтесь /start для ознакомления с списком комманд",
+            { reply_markup: { remove_keyboard: true } }
+          );
         }
-        checkState(chatId,text)
-        break
-  }
+        checkState(chatId, text);
+        break;
+    }
   } catch (error) {
-    console.log("Error: " + error)
-    bot.sendMessage(chatId, 'Возникла ошибка: \nВозможно во время выполнения запроса были неполадки на стороне телеграмма \n/result - чтобы повторно отослать запрос');
+    console.log("Error: " + error);
+    bot.sendMessage(
+      chatId,
+      "Возникла ошибка: \nВозможно во время выполнения запроса были неполадки на стороне телеграмма \n/result - чтобы повторно отослать запрос"
+    );
   }
 });
--- pdfchanger.js
+++ pdfchanger.js
@@ -1,68 +1,46 @@
-const {PDFDocument, PDFForm, StandardFonts, PDFFont} = require('pdf-lib');
-const fontkit = require('@pdf-lib/fontkit');
-const fs = require("fs")
+const { PDFDocument, PDFForm, StandardFonts, PDFFont } = require("pdf-lib");
+const fontkit = require("@pdf-lib/fontkit");
+const fs = require("fs");
 
 async function replaceText(inputFileName, outputFileName, replacements) {
-
-  function findReplacement(formName){
+  function findReplacement(formName) {
     let res = null;
-    replacements.forEach(element =>{
-      if(element.find == formName){
-        res = element.replace
+    replacements.forEach((element) => {
+      if (element.find == formName) {
+        res = element.replace;
       }
-    })
+    });
     return res;
-  }  
+  }
 
-  const formPdfBytes = fs.readFileSync(inputFileName)
-  const pdfDoc = await PDFDocument.load(formPdfBytes)
+  const formPdfBytes = fs.readFileSync(inputFileName);
+  const pdfDoc = await PDFDocument.load(formPdfBytes);
   pdfDoc.registerFontkit(fontkit); // Для кастомного шрифту
   const fontBytes = fs.readFileSync("./static/Montserrat-Regular.ttf");
   const customFont = await pdfDoc.embedFont(fontBytes);
 
   const form = pdfDoc.getForm();
 
-
   // Перевірка які поля для вводу є у PDF файлі та їх зміна
-  const fields = form.getFields()
-  fields.forEach(field => {
-    const name = field.getName()
-    const change_field = form.getTextField(name)
-    // const type = field.constructor.name
-    // console.log(`${type}: ${name}`)
-
-    // Невеликий костиль
-    if(name == "currDate"){
-      change_field.setText((new Date()).toLocaleDateString('en-GB'))
-    }else if(name == "id"){
-      change_field.setText(Math.floor(Math.random() * (999 - 100 + 1)) + 100+"382472893")
-    }else{
-      change_field.setText(findReplacement(name))
+  const fields = form.getFields();
+  fields.forEach((field) => {
+    const name = field.getName();
+    const change_field = form.getTextField(name);
+    if (name == "currDate") {
+      change_field.setText(new Date().toLocaleDateString("en-GB"));
+    } else if (name == "id") {
+      change_field.setText(
+        Math.floor(Math.random() * (999 - 100 + 1)) + 100 + "382472893"
+      );
+    } else {
+      change_field.setText(findReplacement(name));
     }
-    // change_field.setFontSize(11);
     change_field.updateAppearances(customFont);
-  })
-
-  // ПІСЛЯ ПРАВОК ЦЯ ЧАСТИНА КОДУ НЕ ПОТРІБНА
-
-  // Вставка зображення до документу
-
-  // const imageBytes = fs.readFileSync(photoName)
-  // const image = await pdfDoc.embedPng(imageBytes)
-
-  // const { width: imgWidth, height: imgHeight } = image.scaleToFit(105,125)
-
-  //   // Вставка зображення по координатам на першу сторінку
-  //   pdfDoc.getPages()[0].drawImage(image, {
-  //     x: 475,
-  //     y: 590,
-  //     width: imgWidth,
-  //     height: imgHeight,
-  //   });
-
-  const pdfBytes = await pdfDoc.save()
+  });
+  form.flatten();
+  const pdfBytes = await pdfDoc.save();
 
   return fs.writeFileSync(outputFileName, pdfBytes);
 }
 
-module.exports = {replaceText}
+module.exports = { replaceText };
